aa6049729f5b8b328f306dc500355f7e6e17386e
JustOssa <rgsoussama@gmail.com>
2023-01-09 20:29:00 +0100

    Changes / Sync with client

--- src/main/java/me/oussa/ensaschat/common/ServerInterface.java
+++ src/main/java/me/oussa/ensaschat/common/ServerInterface.java
@@ -4,13 +4,16 @@ import me.oussa.ensaschat.model.User;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.List;
 
 public interface ServerInterface extends Remote {
     void sendToAll(String message) throws RemoteException;
 
-    void addClient(String clientName ,ClientInterface client) throws RemoteException;
+    void addClient(String clientName, ClientInterface client) throws RemoteException;
 
     void removeClient(String clientName) throws RemoteException;
 
     User signIn(String username, String password) throws RemoteException;
+
+    List<User> getUsers() throws RemoteException;
 }
--- src/main/java/me/oussa/ensaschat/service/ServerService.java
+++ src/main/java/me/oussa/ensaschat/service/ServerService.java
@@ -10,12 +10,12 @@ import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class ServerService extends UnicastRemoteObject implements ServerInterface {
 
     private final ServerController serverController;
 
-    // List of online clients:
     private final HashMap<String, ClientInterface> onlineUsers = new HashMap<>();
 
     public ServerService(ServerController serverController) throws RemoteException {
@@ -23,21 +23,22 @@ public class ServerService extends UnicastRemoteObject implements ServerInterfac
     }
 
 
-    /***************
+    /* *********** *
      * RMI Methods *
-     ***************/
+     * *********** */
 
     /**
      * RMI method to send message to clients,
      * used from the client to send message to the client
+     *
+     * @param message the message to send
      **/
     public void sendToAll(String message) throws RemoteException {
         onlineUsers.forEach((username, client) -> {
             try {
                 client.receiveMessage(message);
             } catch (RemoteException e) {
-                System.out.println("Error sending message to " + username);
-                // e.printStackTrace();
+                e.printStackTrace();
             }
         });
         serverController.printMessage(message.strip());
@@ -55,21 +56,27 @@ public class ServerService extends UnicastRemoteObject implements ServerInterfac
         // We can't update the UI from a non-JavaFX thread,
         // so we use Platform.runLater to run it on the JavaFX thread
         Platform.runLater(() -> serverController.updateClientsCount(onlineUsers.size()));
-        sendClientsList();
+        sendOnlineClientsList();
     }
 
     /**
      * RMI method to disconnect client to server,
-     * used from the client to remove client instance from the server
-     * and send the updated clients list to all clients
+     * used from the client to remove client instance from the server,
+     * update clients count and send the updated clients list to all clients
      **/
     public void removeClient(String clientName) throws RemoteException {
         onlineUsers.remove(clientName);
         serverController.printMessage("[-] " + clientName + " disconnected");
         Platform.runLater(() -> serverController.updateClientsCount(onlineUsers.size()));
-        sendClientsList();
+        sendOnlineClientsList();
     }
 
+    /**
+     * RMI method to check username/password validity,
+     * used from the client to sign in user
+     *
+     * @return the signed-in user
+     **/
     @Override
     public User signIn(String username, String password) throws RemoteException {
         // already validated in client, but why not
@@ -79,24 +86,38 @@ public class ServerService extends UnicastRemoteObject implements ServerInterfac
 
         // TODO: replace with database calls
         if (username.equals("admin") && password.equals("admin")) {
-            return new User(username, password);
+            return new User(username);
         }
         if (username.equals("user") && password.equals("user")) {
-            return new User(username, password);
+            return new User(username);
         }
         return null;
     }
 
+    /**
+     * RMI method to get all users from the db,
+     *
+     * @return list of all users
+     **/
+    // TODO: fetch users from db
+    public List<User> getUsers() {
+        ArrayList<User> users = new ArrayList<>();
+        users.add(new User("admin"));
+        users.add(new User("user"));
+        users.add(new User("test"));
+        return users;
+    }
 
-    /*******************
+
+    /* *************** *
      * Non-RMI methods *
-     *******************/
+     * *************** */
 
-    // send clients list to all clients
-    private void sendClientsList() {
+    // send online clients list to all clients
+    private void sendOnlineClientsList() {
         onlineUsers.forEach((username, client) -> {
             try {
-                client.updateClientsList(new ArrayList<>(onlineUsers.keySet()));
+                client.updateOnlineUsers(new ArrayList<>(onlineUsers.keySet()));
             } catch (RemoteException e) {
                 e.printStackTrace();
             }
